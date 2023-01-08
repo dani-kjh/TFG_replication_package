f59cbab6bc1c1e0c2e37646a227e79ac5aaf604d
Daniel Escribano <daniel.escribano@hyds.es>
2023-01-08 10:35:42 +0100

    wip: add summary and latency summary table, modify provider name in results.csv

--- reports/reports.py
+++ reports/reports.py
@@ -8,12 +8,15 @@ import scikit_posthocs as sp
 import cliffs_delta as cd
 
 
-def generate_summary(data):
+def generate_summary(data, lat_data):
     summary_emissions_data = [["", "", "min", "max", "mean", "median", "std"]]
+    summary_latency_data = [["", "", "min", "max", "mean", "median", "std"]]
+    summary_data = [["prov", "cpu_m", "cpu_c", "reg", "cntr", "em (g)", "comp (s)", "lat (s)"]]
+
     mult = 1000
     for provider in data:
         new_row = [
-            "emissions (g)",
+            "em (g)",
             provider.cloud_provider[0],
             provider.emissions.min() * mult,
             provider.emissions.max() * mult,
@@ -22,27 +25,43 @@ def generate_summary(data):
             provider.emissions.std() * mult,
         ]
         summary_emissions_data.append(new_row)
+
+        summary_latency_data.append(
+            [
+                "latency (s)",
+                provider.cloud_provider[0],
+                lat_data[provider.cloud_provider[0]].min(),
+                lat_data[provider.cloud_provider[0]].max(),
+                lat_data[provider.cloud_provider[0]].mean(),
+                lat_data[provider.cloud_provider[0]].median(),
+                lat_data[provider.cloud_provider[0]].std(),
+            ]
+        )
+
+        summary_data.append(
+            [
+                provider.cloud_provider[0],
+                provider.cpu_model[0],
+                provider.cpu_count[0],
+                provider.region[0],
+                provider.country_name[0],
+                provider.emissions.mean() * mult,
+                provider.duration.mean(),
+                lat_data[provider.cloud_provider[0]].mean(),
+            ]
+        )
+
     with open("report_figures/summary_emissions.html", "w") as f:
         f.write(tabulate(summary_emissions_data, headers="firstrow", tablefmt="html"))
-    summary_energy_data = [["", "", "min", "max", "mean", "median", "std"]]
-    for provider in data:
-        new_row = [
-            "en (W)",
-            provider.cloud_provider[0],
-            provider.energy_consumed.min() * mult,
-            provider.energy_consumed.max() * mult,
-            provider.energy_consumed.mean() * mult,
-            provider.energy_consumed.median() * mult,
-            provider.energy_consumed.std() * mult,
-        ]
-        summary_energy_data.append(new_row)
-    with open("report_figures/summary_energy.html", "w") as f:
-        f.write(tabulate(summary_energy_data, headers="firstrow", tablefmt="html"))
+    with open("report_figures/summary_lat.html", "w") as f:
+        f.write(tabulate(summary_latency_data, headers="firstrow", tablefmt="html"))
+    with open("report_figures/summary.html", "w") as f:
+        f.write(tabulate(summary_data, headers="firstrow", tablefmt="html"))
 
 
 def gen_normality_results(providers):
     sns.set(style="darkgrid")
-    my_pal = {"aws": "skyblue", "azure": "olive", "heroku": "gold", "gcp": "teal"}
+    my_pal = {"aws": "skyblue", "azure": "olive", "heroku": "gold", "railway": "teal"}
     df = pd.concat(providers, ignore_index=True)
     violin = sns.violinplot(x="cloud_provider", y="energy_consumed", data=df, scale="width", palette=my_pal)
     figure = violin.get_figure()
@@ -130,6 +149,11 @@ aws = "../results/aws/2023-01-06-13.05.23/API_results.csv"
 railway = "../results/railway//2023-01-06-13.24.06/API_results.csv"
 heroku = "../results/heroku/2023-01-06-13.15.31/API_results.csv"
 
+azure_lat = "../results/azure/2023-01-06-13.01.49/latency_results.csv"
+aws_lat = "../results/aws/2023-01-06-13.05.23/latency_results.csv"
+railway_lat = "../results/railway//2023-01-06-13.24.06/latency_results.csv"
+heroku_lat = "../results/heroku/2023-01-06-13.15.31/latency_results.csv"
+
 
 sns.set(style="darkgrid")
 azure = pd.read_csv(azure)
@@ -137,8 +161,16 @@ aws = pd.read_csv(aws)
 heroku = pd.read_csv(heroku)
 railway = pd.read_csv(railway)
 
-results = [azure, aws, heroku, railway]
+azure_lat = pd.read_csv(azure_lat)
+aws_lat = pd.read_csv(aws_lat)
+heroku_lat = pd.read_csv(heroku_lat)
+railway_lat = pd.read_csv(railway_lat)
 
+
+results = [aws, azure, heroku, railway]
+results_lat = {"aws": aws_lat, "azure": azure_lat, "heroku": heroku_lat, "railway": railway_lat}
+
+generate_summary(results, results_lat)
 gen_normality_results(results)
 
 test_significance(results)
